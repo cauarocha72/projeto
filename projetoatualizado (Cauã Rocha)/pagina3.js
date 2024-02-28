408c189786321752a8e4e8e2630eca6eab9cb273
cauarocha72 <150359289+cauarocha72@users.noreply.github.com>
2024-02-28 08:30:04 -0300

    Add files via upload

---
+++ projetoatualizado (Cauã Rocha)/pagina3.js
@@ -0,0 +1,12 @@
+function especificar1(){
+ location.href="especificação.html"
+}
+function especificar2(){
+    location.href="especificação2.html"
+   }
+   function especificar3(){
+    location.href="especificação3.html"
+   }
+   function especificar4(){
+    location.href="especificação4.html"
+   }
